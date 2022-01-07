f7b079e2fc66902e836df8c4a89ea0c2d26883a8
Jae Lo Presti (DN0) <me@jae.fi>
2022-01-07 13:42:38 +0200

    Main: handle gitea_api (#9)

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ logger = logging.getLogger('main')
 # Endpoints
 from handlers.gitlab import gitlab_api
 from handlers.github import github_api
+from handlers.gitea import gitea_api
 
 # Main app
 app = Flask(__name__)
@@ -15,6 +16,7 @@ app = Flask(__name__)
 # Endpoints
 app.register_blueprint(gitlab_api)
 app.register_blueprint(github_api)
+app.register_blueprint(gitea_api)
 
 if __name__ == '__main__':
     app.run()
