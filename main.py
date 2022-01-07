e4af416c159977eb0829c4f3ebe9cb5b537606e9
Jae Lo Presti (DN0) <me@jae.fi>
2022-01-07 18:20:01 +0200

    Main: add scheduler to auto-join rooms (fixes #1)

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import logging
 
+from flask_apscheduler import APScheduler
 from flask import Flask
+
+# Flask config
+class Config:
+    SCHEDULER_TIMEZONE = 'Europe/Helsinki'
 
 # Setup logging
 logger = logging.getLogger('main')
+
+# Scheduler
+from utils.roomutil import check_matrix_rooms_for_joins
 
 # Endpoints
 from handlers.gitlab import gitlab_api
@@ -12,6 +20,18 @@ from handlers.gitea import gitea_api
 
 # Main app
 app = Flask(__name__)
+app.config.from_object(Config())
+
+# Scheduler
+scheduler = APScheduler()
+scheduler.init_app(app)
+scheduler.start()
+
+# Add scheduler to auto-join Matrix rooms
+@scheduler.task('interval', id='join_rooms', seconds=15)
+def join_rooms():
+    logger.warn('Running the join_rooms task.')
+    check_matrix_rooms_for_joins()
 
 # Endpoints
 app.register_blueprint(gitlab_api)
