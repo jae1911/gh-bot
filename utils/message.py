c209c4c691b6d0ca6ef7cc035d1272c4c1a1b263
Jae Lo Presti (DN0) <me@jae.fi>
2022-01-13 18:11:34 +0200

    Message util: add a way to silently log webhook events to room

--- utils/message.py
+++ utils/message.py
@@ -8,6 +8,29 @@ import markdown
 MATRIX_TOKEN = os.environ.get('MATRIX_TOKEN')
 MATRIX_HOMESERVER = os.environ.get('MATRIX_HOMESERVER')
 
+# Method to log events to rooms
+def log_event_to_rooms(event=None, webhooktype='generic'):
+    if not MATRIX_HOMESERVER or not MATRIX_TOKEN or not event:
+        return
+
+    payload = {
+        'msgtype': f'fi.jae.webhook.{webhooktype}',
+        'body': event,
+    }
+
+    r = requests.get(f'https://{MATRIX_HOMESERVER}/_matrix/client/v3/joined_rooms?access_token={MATRIX_TOKEN}')
+
+    if r.status_code != 200:
+        log.error(f'Something bad happened: {r.text}')
+        return
+
+    joined_rooms = json.loads(r.text)
+    for room in joined_rooms.get('joined_rooms'):
+        msg = f'https://{MATRIX_HOMESERVER}/_matrix/client/r0/rooms/{room}/send/fi.jae.webhooklog?access_token={MATRIX_TOKEN}'
+        r = requests.post(msg, data=json.dumps(payload))
+        if r.status_code != 200:
+            log.error(f'Something bad happened: {r.text}')
+
 # Function to send a message
 def send_to_matrix(message=None):
     if not MATRIX_HOMESERVER or not MATRIX_TOKEN or not message:
